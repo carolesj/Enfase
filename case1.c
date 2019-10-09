9b944cb0158639cb9252f9e47c81ac5235d0d212
carolesj <carolinejesuino.ns@gmail.com>
2019-10-08 21:35:02 -0300

    Improved documentation and removed unused variables

--- case1.c
+++ case1.c
@@ -9,8 +9,8 @@ Since this case is simple and little, I opted to not overmodularize it
 int walk_torwards_each_other(int first_initial_position, int
 	second_initial_position) {
 	
-	int position_difference, tiredness = 0, 
-		first_tiredness_coeficient = 1, second_tiredness_coeficient = 1;
+	int tiredness = 0, first_tiredness_coeficient = 1, 
+		second_tiredness_coeficient = 1;
 	
 	// to make sure the orientation is right, the first friend will always
 	// be in the smallest position
@@ -21,19 +21,23 @@ int walk_torwards_each_other(int first_initial_position, int
 	}
 	
 	// at every iteration we have a scenario where at least one of the
-	// initial positions are changed. The idea is that, everytime a friend
-	// moves, we have almost the same situation, but only the tirednesses
-	// are different
+	// initial positions are changed.
 	while(forever) {
+		// the first friend takes a step, gets more tired and his
+		// coeficient increases
 		first_initial_position++;
 		tiredness += first_tiredness_coeficient;
 		first_tiredness_coeficient++;
+		//if the friends met, we achieved our goal
 		if (first_initial_position == second_initial_position) {
 			break;
 		}
+		// if the friends did not meet, the second friend takes a step,
+		// also getting more tired and with a bigger coeficient
 		second_initial_position--;
 		tiredness += second_tiredness_coeficient;
 		second_tiredness_coeficient++;
+		// if the friends met, we achieved our goal
 		if (first_initial_position == second_initial_position) {
 			break;
 		}	
